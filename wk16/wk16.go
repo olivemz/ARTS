3c6bd70d291715c55ca791575dd2e2bd444188b3
olivemz <zhuchensong@gmail.com>
2020-03-04 21:42:33 +1100

    wk16 some code

---
+++ wk16/wk16.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func generateParenthesis(n int) []string {
+	ans := make([]string, 0)
+	backTrack(&ans, "", 0, 0, n)
+	return ans
+}
+func backTrack(ans *[]string, cur string, open int, close int, max int) {
+	if len(cur) == 2*max {
+		*ans = append(*ans, cur)
+		return
+	}
+	if open < max {
+		backTrack(ans, cur+"(", open+1, close, max)
+	}
+	if close < open {
+		backTrack(ans, cur+")", open, close+1, max)
+	}
+}
+func main() {
+	x := generateParenthesis(2)
+	fmt.Println(x)
+}
